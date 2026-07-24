9522b28f20ab143cf78483b79aa554378129b356
ma7m0odsaber <rodomahmoud121@gmail.com>
2026-07-24 18:45:57 +0300

    completing defined rules

--- main.cpp
+++ main.cpp
@@ -2,29 +2,60 @@
 
 using namespace std;
 
-class Irule
+enum class car_type
 {
-public:
-    virtual ~Irule() = default;
-    virtual bool check_rule() = 0;
-};
-
-class truck_speed: public Irule
-{
-public:
-    bool check_rule() override
-    {
-
-    }
+    Private,
+    Truck,
+    Bus
 };
 
 struct observation
 {
     string plate_num;
     string date;
-    string car_type;
+    car_type car;
     int speed;
     bool seat_belt;
+};
+
+class Irule
+{
+public:
+    virtual ~Irule() = default;
+    virtual bool check_violation(observation obs) = 0;
+};
+
+class truck_speed: public Irule
+{
+public:
+    bool check_violation(observation obs) override
+    {
+        if (obs.car == car_type::Truck && obs.speed > 60)
+            return true;
+        return false;
+    }
+};
+
+class private_speed: public Irule
+{
+public:
+    bool check_violation(observation obs) override
+    {
+        if (obs.car == car_type::Private && obs.speed > 80)
+            return true;
+        return false;
+    }
+};
+
+class seatbelt: public Irule
+{
+public:
+    bool check_violation(observation obs) override
+    {
+        if (!obs.seat_belt)
+            return true;
+        return false;
+    }
 };
 
 int main()
